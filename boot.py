4a2a1eff2bb0f84d1e99a0ff0e1f066fbd32cc07
Prof.LoL <waradteni@gmail.com>
2026-04-29 23:45:19 +0530

    Fix Wi-Fi SSID and password formatting

--- boot.py
+++ boot.py
@@ -1,14 +1,19 @@
 import network
 import esp
 import time
+
 esp.osdebug(None)
-ssid = '404_Not_Found'
-password = '403_Forbidden'
+
+ssid = '404 Not Found'
+password = '403'
+
 # Connect to Wi-Fi
 station = network.WLAN(network.STA_IF)
 station.active(True)
 station.connect(ssid, password)
+
 while not station.isconnected():
-time.sleep(1)
+    time.sleep(1)
+
 print('Connection successful')
 print(station.ifconfig())
